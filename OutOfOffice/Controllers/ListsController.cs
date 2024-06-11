ad7a185a8b7948ad57ceed41df7a421099e42006
Berdnyk-O <sacha.berdnuk@gmail.com>
2024-06-11 16:53:44 +0300

    sort table rows using sorting in the column headers

--- OutOfOffice/Controllers/ListsController.cs
+++ OutOfOffice/Controllers/ListsController.cs
@@ -13,9 +13,32 @@ namespace OutOfOffice.Controllers
             _manager = manager;
         }
 
-        public async Task<ActionResult> Employees()
+        public async Task<ActionResult> Employees(string sortBy)
         {
             var employees = await _manager.GetEmployeesAsync();
+
+            switch(sortBy)
+            {
+                case "FullName":
+                    employees = employees.OrderByDescending(x => x.FullName).ToList();
+                    break;
+                case "Subdivision":
+                    employees = employees.OrderByDescending(x => x.Subdivision).ToList();
+                    break;
+                case "Position":
+                    employees = employees.OrderByDescending(x => x.Position).ToList();
+                    break;
+                case "Status":
+                    employees = employees.OrderByDescending(x => x.Status).ToList();
+                    break;
+                case "Partner":
+                    employees = employees.OrderByDescending(x => x.PeoplePartnerId).ToList();
+                    break;
+                case "OutOfOfficeBalance":
+                    employees = employees.OrderByDescending(x => x.OutOfOfficeBalance).ToList();
+                    break;
+            }
+
             return View(employees);
         }
 
