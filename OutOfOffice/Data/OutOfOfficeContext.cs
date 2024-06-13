3732c3cc9dcfc727cb201c97352b1f156c4cd6f6
Berdnyk-O <sacha.berdnuk@gmail.com>
2024-06-13 14:28:28 +0300

    add application users to the database

--- OutOfOffice/Data/OutOfOfficeContext.cs
+++ OutOfOffice/Data/OutOfOfficeContext.cs
@@ -9,6 +9,7 @@ namespace OutOfOffice.Data
         public DbSet<LeaveRequest> LeaveRequests { get; set; }
         public DbSet<ApprovalRequest> ApprovalRequests { get; set; }
         public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public OutOfOfficeContext(DbContextOptions<OutOfOfficeContext> options)
             : base(options)
